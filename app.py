f06e6e38a9406b6da9a9c33b15cb7c776df2a602
mayayann <s.mayayann.2010@gmail.com>
2024-10-02 21:03:32 +0900

    add html

--- app.py
+++ app.py
@@ -9,5 +9,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+@app.route("/family")
+def family():
+    return render_template("family.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 
 app.run(debug=True,host="0.0.0.0")
