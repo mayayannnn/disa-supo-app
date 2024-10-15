7d7c73306a26e256eb90fbff4df3975e6dede83b
kbanchi1111 <kbanchi.1111@gmail.com>
2024-10-16 01:10:18 +0900

    https ssl

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def profile():
     return render_template("profile.html", liff_id=liff_id)
 
 
-app.run(debug=True,host="0.0.0.0", port=5001)
+app.run(debug=True,host="0.0.0.0", port=5001, ssl_context=('ssl/cert.pem', 'ssl/private.key'))
