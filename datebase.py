9b653845a86271989ccfbf11d459711f53e96473
mayayann <s.mayayann.2010@gmail.com>
2024-10-15 21:20:27 +0900

    add database

--- datebase.py
+++ datebase.py
@@ -12,4 +12,63 @@ class User(Model):
     class Meta:
         database = db
     
-db.create_tables([User])
+class Family(Model):
+    family_id = CharField()
+    user = ForeignKeyField(User,backref="family")
+
+    class Meta:
+        database = db
+
+class Shelter(Model):
+    name = CharField()
+    address = CharField()
+    category = CharField()
+    capacity = CharField()
+    phone_number = CharField()
+
+    class Meta:
+        database = db
+
+class Hospital(Model):
+    name = CharField()
+    address = CharField()
+    category = CharField()
+    capacity = CharField()
+    phone_number = CharField()
+
+    class Meta:
+        database = db
+
+class Pharmacy(Model):
+    name = CharField()
+    address = CharField()
+    capacity = CharField()
+    phone_number = CharField()
+
+    class Meta:
+        database = db
+
+class ReliefSuppliesCateogy(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class ReliefSupplies(Model):
+    shelter = ForeignKeyField(Shelter,backref="reliefsupplies")
+    reliefsuppliescateogy = ForeignKeyField(ReliefSuppliesCateogy,backref="reliefsuppliescateogy")
+    required_number = IntegerField()
+    used_number = IntegerField()
+
+
+    class Meta:
+        database = db
+
+class Knowledge(Model):
+    question = CharField()
+    answwer = CharField()
+
+    class Meta:
+        database = db
+
+db.create_tables([User,Family])
