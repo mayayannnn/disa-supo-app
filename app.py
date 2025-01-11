16d0067efda8e3e61b28b8b8a0e0dae01979649f
mayayann <s.mayayann.2010@gmail.com>
2025-01-11 21:14:33 +0900

    no

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def family():
         user = User.select().where(User.line_id == line_id).first()
     if not user:
         return redirect(url_for('profile'))
-    families = Family.select(Family,User,UserPosition).join(User,on=Family.to_user == User.id).switch(User).join(UserPosition,on=User.id == UserPosition.user_id).where(Family.from_user == user.id)
+    families = Family.select().where(Family.from_user == user.id)
     family_invitation = FamilyInvitation.select().where(FamilyInvitation.invite_user == user.id).first()
     if not family_invitation:
         FamilyInvitation.create(invite_user=user.id, code=uuid.uuid4(), created_at=datetime.now())
